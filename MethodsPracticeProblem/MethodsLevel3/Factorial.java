a2992d9ca59c0d5105e931ed05f4611e4ca169e6
*Love447-png <*varunkhandelwal16nov@gmail.com>
2025-09-08 22:50:53 +0530

    files saved

---
+++ MethodsPracticeProblem/MethodsLevel3/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Factorial {
+    public static int factRec(int n){
+        if(n<=1) return 1;
+        return n*factRec(n-1);
+    }
+    public static int factItr(int n){
+        int f=1;
+        for(int i=1;i<=n;i++) f*=i;
+        return f;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Rec="+factRec(n));
+        System.out.println("Itr="+factItr(n));
+    }
+}
